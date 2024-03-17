cb9fd1fdffafaaafad1a2fe2f1b3e7b247d31a28
Igor Vicente <79584321+igorvicente0712@users.noreply.github.com>
2024-03-17 12:41:10 -0300

    Update atp.py

--- aula04/atp.py
+++ aula04/atp.py
@@ -25,29 +25,43 @@ for i in range(len(estados)):
     #transicoes[transicao[0]][alfabetos[j]] = transicao[j+1]
 
 imputi = input()
-
 caminhos = [primeiro_estado]
-
+print(caminhos)
 for char in imputi:
+    print(char)
     if char not in alfabetos:
         print("rejeita")
         break
     n_caminho = 0
+    tam_caminhos = len(caminhos)
     while n_caminho < len(caminhos):
         # COLOCAR SITUAÇÃO DE EPSILON VULGO PASSAR PARA OS OUTROS ESTADOS INDEPENDENTE
-        if transicoes[f"{caminhos[n_caminho]}"][f"{char}"] == "vazio":
-            del caminhos[i]
+        # print(n_caminho, caminhos[n_caminho], char)
+        proximos = transicoes[caminhos[n_caminho]][char].split(",")
+        # proximos_vazio = transicoes[caminhos[n_caminho]]["vazio"].split(",")
+        # for estado in proximos_vazio:
+        #     if estado == "vazio":
+        #         break
+        #     else:
+        #         proximos.append(transicoes[estado][char])
+        # for estado in proximos:
+        if proximos[0] == "vazio":
+            del caminhos[n_caminho]
         else:
-            proximos = transicoes[f"{caminhos[i]}"][f"{char}"].split(",")
+            #proximos = transicoes[caminhos[n_caminho]][char].split(",")
+            caminhos[n_caminho] = proximos[0]
             if len(proximos) > 1:
                 for i in range(1, len(proximos)):
-                    caminhos.insert(n_caminho, proximo)
+                    caminhos.insert(n_caminho + 1, proximos[i])
+                    n_caminho = n_caminho + 1
             n_caminho = n_caminho + 1
-            proximos_vazio = transicoes[f"{caminhos[i]}"][f"vazio"].split(",")
+        tam_caminhos = len(caminhos)
+        # sleep(1)
+    print(caminhos)
 else:
     for estado_caminho in caminhos:
         if estado_caminho in estados_aceitos:
             print("aceita")
             break
-        else:
-            print("rejeita")
+    else:
+        print("rejeita")
